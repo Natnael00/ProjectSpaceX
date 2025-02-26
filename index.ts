81502d83d549b1eb6b8e477c51f44ae0044c19ef
Natnael <natnael@telenet.be>
2025-02-26 15:26:51 +0000

    Laatste wijzigingen committen

---
+++ index.ts
@@ -0,0 +1,20 @@
+// Oefening: Interfaces en Objecten - SpaceX Edition
+// Doel: Leer hoe je interfaces gebruikt om objecten te structureren
+
+// 1️⃣ Stap 1: Maak een interface voor een raket
+// Een raket heeft een naam, een fabrikant en een lanceerdatum
+
+
+interface Raket{
+    naam: string;
+    fabrikant: string;
+    lanceerDatum: string;
+}
+
+const falcon9: Raket = {
+
+naam:"Falcon9",
+fabrikant:"SpaceX",
+lanceerDatum:"2013-03-15"
+
+};
